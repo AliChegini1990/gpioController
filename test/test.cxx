a5a81e81443ddb80a77f08fc16f84b8221564cc0
alireza chegini <ali.chegini.1990@gmail.com>
2022-06-15 08:02:48 +0430

    test.cxx: Add a test

--- test/test.cxx
+++ test/test.cxx
@@ -1,1 +1,27 @@
-// write some tests
+#include "gpioController.h"
+#include <cassert>
+#include <iostream>
+#include <stdexcept>
+
+#define massert(MSG, EX) assert((MSG, EX))
+using namespace gpio_controller;
+
+void test1() {
+  try {
+
+    GpioController gp1;
+    GpioController gp2;
+
+    // gp1 = gp2; // Error, they are not copyable
+
+    gp1.AddController(unique_ptr<IController>(new ToogleBtn()));
+    gp1.AddController(unique_ptr<IController>(new SystemSleepController()));
+  } catch (const std::exception &ex) {
+    cerr << ex.what() << endl;
+  }
+}
+
+int main() {
+  test1();
+  return 0;
+}
